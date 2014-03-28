f13812d0f6412ddb3924345a5707088fa4d000a2
Mikola Lysenko <mikolalysenko@gmail.com>
2014-03-28 15:48:35 -0500

    adding package.json

---
+++ test/test.js
@@ -0,0 +1,11 @@
+"use strict"
+
+var preprocessRegions = require("../region.js")
+var tape = require("tape")
+
+tape("point-in-region", function(t) {
+
+  //TODO: Add test cases here
+
+  t.end()
+})
